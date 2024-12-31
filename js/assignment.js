2222baf5d5f1619285b2db22ce693735c9771128
Jason Caldwell <jman8851@gmail.com>
2024-12-30 21:44:52 -0500

    updated Readme as completed assignments. commented out code in the index.js and compiled code in the assignment.js per instructions to generated expected answers on assignment.

--- js/assignment.js
+++ js/assignment.js
@@ -6,3 +6,24 @@ const checkIfEvenElement = document.getElementById("check-if-even");
 const lostNumbersElement = document.getElementById("lost-numbers");
 
 const lostNumbers = [4, 8, 15, 16, 23, 42];
+
+function returnTheRemainder(a, b) {
+    return a % b;
+}
+
+function checkIfEven(num) {
+    return num % 2 === 0;
+}
+
+function getTheFourthElement (arr) {
+    return arr[3];
+}
+
+function renderResults () {
+    remainderElement.textContent = `${returnTheRemainder(5, 4)}`;
+    checkIfEvenElement.textContent = `${checkIfEven(47)}`;
+    lostNumbersElement.textContent = `${getTheFourthElement(lostNumbers)}`;
+}
+
+
+submissionBtn.addEventListener("click", renderResults);
--- js/index.js
+++ js/index.js
@@ -1,4 +1,18 @@
 // Prevent us from attempting to use variables
 // that are not declared
-"use strict"
+//"use strict"
+//let sum = add(2, 100);
+
+//console.log(sum);
+
+
+//const greet = (name) => {
+  //  return 'Hello, ' + name + '!';
+
+
+//};
+
+//function greet() {
+  //  console.log('Hello, World!');
+//}
 
